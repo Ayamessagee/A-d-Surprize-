f36b91e9a0f2589f950dd6f63286294865af719e
Ayamessagee <ayoub112233b@gmail.com>
2026-03-13 16:27:47 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const yesBtn = document.getElementById('yes');
+const noBtn = document.getElementById('no');
+const screen1 = document.getElementById('screen1');
+const screen2 = document.getElementById('screen2');
+
+yesBtn.addEventListener('click', () => {
+  screen1.style.display = 'none';
+  screen2.style.display = 'block';
+});
+
+// زر No يتحرك بدل الاختفاء
+noBtn.addEventListener('mouseover', () => {
+  const maxX = screen1.clientWidth - noBtn.offsetWidth;
+  const maxY = screen1.clientHeight - noBtn.offsetHeight;
+  const randX = Math.floor(Math.random() * maxX);
+  const randY = Math.floor(Math.random() * maxY);
+  noBtn.style.position = 'absolute';
+  noBtn.style.left = randX + 'px';
+  noBtn.style.top = randY + 'px';
+});
